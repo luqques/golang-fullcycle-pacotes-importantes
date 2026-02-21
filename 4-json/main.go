1292c1d4b342c6c86933c79bde6a64872c3a599f
Lucas da Cunha <lucas.da.cunha.dev@gmail.com>
2026-02-21 18:49:46 -0300

    notations e tratamentos com json

--- 4-json/main.go
+++ 4-json/main.go
@@ -1,17 +1,33 @@
 package main
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"os"
+)
 
 type Conta struct {
-	Numero int
-	Saldo  float64
+	Numero int     `json:"numero_conta"`
+	Saldo  float64 `json:"saldo_conta"`
 }
 
 func main() {
 	conta := Conta{Numero: 12345, Saldo: 1000.50}
-	json, err := json.Marshal(conta)
+	res, err := json.Marshal(conta)
 	if err != nil {
 		panic(err)
 	}
-	println(string(json))
+	println(string(res))
+
+	err = json.NewEncoder(os.Stdout).Encode(conta)
+	if err != nil {
+		panic(err)
+	}
+
+	jsonPuro := []byte(`{"numero_conta":67890,"saldo_conta":2500.75}`)
+	var outraConta Conta
+	err = json.Unmarshal(jsonPuro, &outraConta)
+	if err != nil {
+		panic(err)
+	}
+	println(outraConta.Numero, outraConta.Saldo)
 }
