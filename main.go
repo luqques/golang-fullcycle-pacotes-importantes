9c4daaaffd0b3f97bba5b797e249ea958eb890e6
Lucas da Cunha <lucas.da.cunha.dev@gmail.com>
2026-02-21 18:25:12 -0300

    criando, lendo e removendo arquivos

--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello WOrld")
+	f, err := os.Create("arquivo.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	tamanho, err := f.Write([]byte("Escrevendo dados no arquivo"))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Bytes escritos:", tamanho)
+
+	f.Close()
+
+	//leitura
+	arquivo, err := os.ReadFile("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(arquivo))
+
+	//leitura com buffer
+	arquivo2, err := os.Open("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+	reader := bufio.NewReader(arquivo2)
+	buffer := make([]byte, 10)
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			fmt.Println("Erro ao acabar o arquivo:", err)
+			break
+		}
+		fmt.Println(string(buffer[:n]))
+	}
+	arquivo2.Close()
+
+	//removendo arquivo
+	err = os.Remove("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
 }
