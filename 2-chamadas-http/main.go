5135494a0214d664f9b469f76dc3ada00df03e5d
Lucas da Cunha <lucas.da.cunha.dev@gmail.com>
2026-02-21 18:33:22 -0300

    realizando uma chamada http e serializando o body

--- 2-chamadas-http/main.go
+++ 2-chamadas-http/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"io"
+	"net/http"
+)
 
+func main() {
+	response, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+
+	defer response.Body.Close()
 }
