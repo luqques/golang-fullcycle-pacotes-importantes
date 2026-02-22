95b84529db1d54613034b4d20f82cb489654bb4e
Lucas da Cunha <lucas.da.cunha.dev@gmail.com>
2026-02-22 13:52:16 -0300

    templates com arquivos estáticos

--- 9-templates/main.go
+++ 9-templates/main.go
@@ -10,6 +10,8 @@ type Curso struct {
 	CargaHoraria int
 }
 
+type Cursos []Curso
+
 func main() {
 	curso := Curso{"Go", 40}
 	tmp := template.New("CursoTemplate")
@@ -23,4 +25,13 @@ func main() {
 	curso2 := Curso{"Python", 30}
 	tmp2 := template.Must(template.New("CursoTemplate2").Parse("Curso: {{.Nome}} - Carga Horária: {{.CargaHoraria}}"))
 	err = tmp2.Execute(os.Stdout, curso2)
+
+	//com arquivos
+	cursos := Cursos{
+		{"Go", 40},
+		{"Python", 30},
+		{"Java", 50},
+	}
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	err = tmpl.Execute(os.Stdout, cursos)
 }
