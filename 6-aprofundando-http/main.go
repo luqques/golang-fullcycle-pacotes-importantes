ac78cfafadc3507ad8c2eb50b145fa2dc5b0091c
Lucas da Cunha <lucas.da.cunha.dev@gmail.com>
2026-02-22 11:33:28 -0300

    buscando cep por URL http

---
+++ 6-aprofundando-http/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ViaCep struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Estado      string `json:"estado"`
+	Regiao      string `json:"regiao"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func main() {
+	http.HandleFunc("/", BuscaCepHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	viaCep, err := BuscaCep(cepParam)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	responseData, err := json.Marshal(viaCep)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(responseData)
+}
+
+func BuscaCep(cep string) (*ViaCep, error) {
+	response, err := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
+	if err != nil {
+		println("Erro ao buscar CEP:", err)
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		println("CEP não encontrado:", response.Status)
+		return nil, err
+	}
+
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		println("Erro ao ler resposta:", err)
+		return nil, err
+	}
+
+	var viaCep ViaCep
+	err = json.Unmarshal(responseData, &viaCep)
+	if err != nil {
+		println("Erro ao fazer unmarshal:", err)
+		return nil, err
+	}
+
+	return &viaCep, nil
+}
